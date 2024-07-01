994f56c5b1beed42ad237a7ab9ab644618ccb359
Sanika Wani <139634358+sanika0030@users.noreply.github.com>
2024-07-01 19:36:09 +0530

    Add files via upload

---
+++ EncapHW.java
@@ -0,0 +1,25 @@
+public class EncapHW {
+    private int id;
+    private String name;
+    private float salary;
+
+    public int getId() {
+        return id;
+    } 
+    public String getName() {
+        return name;
+    }
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setid(int newId) {
+        id = newId;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
+    public void setSalary(float newSalary) {
+        salary = newSalary;
+    }
+}
---
+++ EncapHWuse.java
@@ -0,0 +1,12 @@
+public class EncapHWuse {
+    public static void main(String[] args) {
+        
+        EncapHW a = new EncapHW();
+
+        a.setid(465154);
+        a.setName("Sakshya Trisila");
+        a.setSalary(8591674);
+
+        System.out.println("Id no.= "+a.getId()+"\nName = "+a.getName()+"\nSalary = "+a.getSalary());
+    }
+}
